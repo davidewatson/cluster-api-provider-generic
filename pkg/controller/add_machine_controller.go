26c7c698d149971fc32ef98691893c23e989abb3
David E Watson <davidewatson@gmail.com>
2019-04-16 14:53:09 -0700

    Remove unused permissions for nonexistant *solas* resources and add permissions for upstream Cluster API Cluster and Machine resources.

--- pkg/controller/add_machine_controller.go
+++ pkg/controller/add_machine_controller.go
@@ -22,7 +22,6 @@ import (
 	"sigs.k8s.io/controller-runtime/pkg/manager"
 )
 
-//+kubebuilder:rbac:groups=solas.k8s.io,resources=solasmachineproviderspecs;solasmachineproviderstatuses,verbs=get;list;watch;create;update;patch;delete
 func init() {
 	// AddToManagerFuncs is a list of functions to create controllers and add them to a manager.
 	AddToManagerFuncs = append(AddToManagerFuncs, func(m manager.Manager) error {
